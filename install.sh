aac1665734a85806d833f9039b56544c4117697c
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-11 18:48:03 +0200

    Create project + setup

---
+++ install.sh
@@ -0,0 +1,20 @@
+react-native init manager
+
+settings.json
+{
+  "editor.tabSize": 2
+}
+
+npm install -g eslint
+npm install --save-dev eslint-config-rallycoding
+
+.eslintrc
+{
+  "extends": "rallycoding"
+}
+
+npm install --save react-redux redux
+npm install --save axios
+npm install --save firebase
+
+react-native run-ios
