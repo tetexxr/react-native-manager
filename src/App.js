812d94c9216b2215488d3e205fed132ee1ca8c30
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-11 18:55:29 +0200

    Install eslint + fix some app + reducers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 class App extends Component {
   render() {
     return (
-      <Provider>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
-                    </Text>
+          </Text>
         </View>
       </Provider>
     );
