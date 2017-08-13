398d5339c413575250cb02535f479309481670f0
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-13 10:08:06 +0200

    Comments

--- install.sh
+++ install.sh
@@ -22,5 +22,13 @@ npm install --save react-native-router-flux@3.35.0
 react-native run-ios
 
 
-rm -rf node_modules && npm install
-rm -rf android/build && rm -rf ios/build
+
+
+1. Clear watchman watches:
+`watchman watch-del-all`
+2. Delete the `node_modules` folder:
+`rm -rf node_modules && npm install`
+3. Reset packager cache:
+`rm -fr $TMPDIR/react-*` or `npm start -- --reset-cache`
+4. Clear build results for iOS and Android:
+`rm -rf android/build && rm -rf ios/build`
