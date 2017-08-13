79f2c23fa92298e7f6401fd3c8d06d6f179a88a6
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-13 08:32:49 +0200

    Reset all properties when success

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -10,12 +10,13 @@ const INITIAL_STATE = {
   email: '',
   password: '',
   user: null,
-  error: ''
+  error: '',
+  loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
   console.log(action);
-  
+
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
@@ -24,7 +25,7 @@ export default (state = INITIAL_STATE, action) => {
     case LOGIN_USER:
       return { ...state, loading: true, error: '' };
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload, error: '', loading: false };
+      return { ...state, ...INITIAL_STATE, user: action.payload };
     case LOGIN_USER_FAIL:
       return { ...state, error: 'Authentication Failed.', password: '', loading: false };
     default:
