6c0fd92ad4e47446e5add1b3d90ba1a8a87161ba
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-16 17:06:30 +0200

    Navigate to list when employee created

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE
 } from './Types';
@@ -12,8 +13,11 @@ export const employeeUpdate = ({ prop, value }) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
-  
-  // Use string interpolation of ES6
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+
+  return () => {
+    // Use string interpolation of ES6
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => Actions.employeeList());
+  };
 };
