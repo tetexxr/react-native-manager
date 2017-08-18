79e334c5f2e5d8d2bdb7e5dec15c1fa257e65606
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-18 19:01:45 +0200

    Delete employee using custom modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Comunnications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button, } from './common';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -25,6 +27,14 @@ class EmployeeEdit extends Component {
     Comunnications.text(phone, `Your upcomming shift is on ${shift}`);
   }
 
+  onAccept() {
+    this.props.employeeDelete(this.props.employee);
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <Card>
@@ -41,6 +51,20 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
@@ -53,5 +77,5 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, {
-  employeeUpdate, employeeSave
+  employeeUpdate, employeeSave, employeeDelete
 })(EmployeeEdit);
